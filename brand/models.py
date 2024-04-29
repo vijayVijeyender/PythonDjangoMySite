eec81bd710ab532e05d1c31a0761e2dabae1cced
kishore kumar <kpcbatman143@gmail.com>
2024-04-29 13:50:12 +0530

    created admin

--- brand/models.py
+++ brand/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Brand (models.Model):
+class Brand(models.Model):
     brandName=models.CharField(max_length=200, null=True)
     brandNameTn=models.CharField(max_length=200, null=True)
     brandDescription=models.CharField(max_length=200, null=True)
